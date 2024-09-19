23b7228e6657d4c9649e4ad938dc5386ad1ea43d
Sambhav Singh <ryugasingh90@gmail.com>
2024-09-19 13:05:25 +0530

    Initial Commit

---
+++ SwiftUICompilstionApp/SwiftUICompilstionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICompilstionAppApp.swift
+//  SwiftUICompilstionApp
+//
+//  Created by Sambhav Singh on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICompilstionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
